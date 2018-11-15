51e00f82b6f0628222bc25f764795ab81d8a4b9a
kuzko58 <41537859+kuzko58@users.noreply.github.com>
2018-11-15 07:32:14 +0100

    update 6

--- UI/script.js
+++ UI/script.js
@@ -1,40 +1,15 @@
 "use strict";
 const userBox = document.getElementById('userBox'),
 	loginButtonMenu = document.getElementById('loginButtonMenu'),
-	weightCat = document.getElementById('weightCat'),
-	catList = document.getElementById('catList'),
 	  userIconBox = document.getElementById('userIconBox'),
 	  userIcon = document.getElementById('userIcon');
 
-catList.addEventListener('click', (event) => {
-	event.stopPropagation();
-	weightCat.style.color = 'black';
-	weightCat.innerHTML = event.target.innerHTML;
-	weightCat.value = event.target.value;
-	catList.classList.remove('listAnimeIn');
-	catList.classList.add('listAnimeOut');
-});
-
-weightCat.addEventListener('click', () => {
-	event.stopPropagation();
-	if (!catList.classList.contains('listAnimeIn')) {
-		catList.classList.remove('listAnimeOut');
-		catList.classList.add('listAnimeIn');
-	} else {
-		catList.classList.remove('listAnimeIn');
-		catList.classList.add('listAnimeOut');
-	}
-});
 
 
-window.addEventListener('click', (event) => {
-	if (!event.target.matches('#weightCat')) {
-		if (catList.classList.contains('listAnimeIn')) {
-			catList.classList.replace('listAnimeIn', 'listAnimeOut');
-		}
+/*window.addEventListener('click', (event) => {
 
 	}
-});
+});*/
 
 loginButtonMenu.addEventListener('click', () => {
 	event.stopPropagation();
